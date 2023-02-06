9fdc690e531fda9586a59a7a8f0d98026f69319e
Goldman <goldmanmine@mail.ru>
2023-02-06 21:29:37 +0300

    3

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
+
+
+K1, K2 = 203.995, -11.998
+
+
+class KeyHelper(QtCore.QObject):
+    keyPressed = QtCore.pyqtSignal(QtCore.Qt.Key)
+
+    def __init__(self, window):
+        super().__init__(window)
+        self._window = window
+
+        self.window.installEventFilter(self)
+
+    @property
+    def window(self):
+        return self._window
+
+    def eventFilter(self, obj, event):
+        if obj is self.window and event.type() == QtCore.QEvent.KeyPress:
+            self.keyPressed.emit(event.key())
+        return super().eventFilter(obj, event)
 
 
 class Example(QMainWindow):
@@ -18,10 +40,11 @@ class Example(QMainWindow):
         self.im = None
 
     def show_map(self):
-        size = int(self.size.value())
+        size = self.size.value()
         c1 = self.c1.value()
         c2 = self.c2.value()
-        request = f'https://static-maps.yandex.ru/1.x/?ll={c1},{c2}&size=450,450&z={size}&l=map'
+        spn = K1 / size + K2
+        request = f'https://static-maps.yandex.ru/1.x/?ll={c1},{c2}&size=450,450&spn={spn},{spn}&l=map'
         response = requests.get(request)
         if response:
             self.err.setText('')
@@ -33,11 +56,25 @@ class Example(QMainWindow):
             self.err.setText('Ошибка')
             print(response.content)
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_PageDown:
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.KeyPress:
+            self.keyPressed.emit(event.key())
+        return super().eventFilter(obj, event)
+
+    def keyPressEvent(self, key):
+        if key == Qt.Key_PageDown:
             self.size.setValue(self.size.value() - 1)
-        if event.key() == Qt.Key_PageUp:
+        if key == Qt.Key_PageUp:
             self.size.setValue(self.size.value() + 1)
+        spn = K1 / self.size.value() + K2
+        if key == Qt.Key_Up:
+            self.c2.setValue(self.c2.value() + spn)
+        if key == Qt.Key_Down:
+            self.c2.setValue(self.c2.value() - spn)
+        if key == Qt.Key_Left:
+            self.c1.setValue(self.c1.value() - spn)
+        if key == Qt.Key_Right:
+            self.c1.setValue(self.c1.value() + spn)
 
 
 def except_hook(cls, exception, traceback):
@@ -48,5 +85,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
+
+    helper = KeyHelper(ex.windowHandle())
+    helper.keyPressed.connect(ex.keyPressEvent)
+
     sys.excepthook = except_hook
     sys.exit(app.exec())
